f3dbfe7d74e15c1dbc42ca3223ed83c7ad6e574d
asd0905 <dydcodydco@daum.net>
2024-07-14 01:00:39 +0900

    Feat: 메인 화면 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,15 @@ export default async function HomePage() {
   const session = await auth(); 
   console.log(session, '--------------------------session');
   return (
-    <div className="flex h-dvh w-full justify-center items-center flex-col">
+    <div className="flex w-full justify-center items-center flex-col">
       <h1 className='font-extrabold text-4xl md:text-9xl -mt-20'>캐치마인드</h1>
-      <div className='flex mt-5 gap-2'>
-        {session?.user?.name && <Button className='p-0'><Link href='/home' className='p-3'>게임하러</Link></Button>}
+      <div className='flex mt-8 gap-2 items-center'>
+        {session?.user?.name && (
+          <>
+            {session?.user?.name}님 환영합니다.
+            <Button className='p-0 ml3'><Link href='/home' className='p-3'>고고</Link></Button>
+          </>
+        )}
         <Login session={session} />
       </div>
     </div>
