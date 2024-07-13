d693cb37143c85ecdfa5d409b4be2a23a87d9a6d
asd0905 <dydcodydco@daum.net>
2024-07-14 00:59:55 +0900

    Feat: 미들웨어 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,14 @@ import { NextRequest, NextResponse } from 'next/server';
 export { auth } from "@/auth";
 
 export async function middleware(request: NextRequest) {
-  console.log(request);
+  console.log('------------------------------------middleware start ------------------------------------------');
+  console.log('------------------------------------middleware end ------------------------------------------');
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    // match all routes except static files and APIs
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
