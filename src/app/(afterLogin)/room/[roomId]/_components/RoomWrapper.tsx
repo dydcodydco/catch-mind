be505815261987f05c313463b328cbbe463bdbca
asd0905 <dydcodydco@daum.net>
2024-07-26 06:04:26 +0900

    Feat: 게임 방 나갈 때 socket room 나가기

--- src/app/(afterLogin)/room/[roomId]/_components/RoomWrapper.tsx
+++ src/app/(afterLogin)/room/[roomId]/_components/RoomWrapper.tsx
@@ -1,19 +1,25 @@
 'use client';
 
 import { socketContext } from '@/app/(afterLogin)/_components/SocketProvider';
+import { Session } from 'next-auth';
 import { usePathname } from 'next/navigation';
 import { ReactNode, useContext, useEffect } from 'react'
 
-type Props = { children: ReactNode };
+type Props = { children: ReactNode, session: Session | null};
 
-export default function RoomWrapper({children}: Props) {
-  const { socket } = useContext(socketContext);
+export default function RoomWrapper({children, session}: Props) {
+  const { socket, setIsModal } = useContext(socketContext);
   const path = usePathname();
-  console.log(path);
 
   useEffect(() => {
-    socket?.emit('join room', 11111111);
-  }, [socket]);
+    socket?.emit('join room', { roomId: path.split('/').pop(), userId: session?.user?.id });
+    
+    return () => {
+      console.log('-------------------------------프론트에서 방나갑니다');
+      socket?.emit('leave room', { roomId: path.split('/').pop(), userId: session?.user?.id });
+      setIsModal(true);
+    }
+  }, [path, session?.user?.id, setIsModal, socket]);
 
   return (
     <>
