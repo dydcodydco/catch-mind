9c19fe5333fa5e92b74a65c308bfad95bd838ea7
asd0905 <dydcodydco@daum.net>
2024-07-25 13:30:29 +0900

    Feat: socket provider 생성/적용

---
+++ src/app/(afterLogin)/_components/SocketProvider.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { Session } from 'next-auth';
+import { disconnect } from 'process';
+import { createContext, ReactNode, useCallback, useEffect, useMemo, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+
+type Props = { children: ReactNode, session: Session | null };
+interface ISocketProps {
+  socket: Socket | null;
+  disconnect: Function;
+  goDown: boolean;
+  setGoDown: Function;
+}
+
+export const socketContext = createContext<ISocketProps>({
+  socket: null,
+  disconnect: () => { },
+  goDown: false,
+  setGoDown: () => {},
+});
+
+
+
+export default function SocketProvider({ children, session }: Props) {
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [goDown, setGoDown] = useState(false);
+  const disconnect = useCallback(() => {
+    socket?.disconnect();
+    setSocket(null);
+  }, [socket]);
+  const contextValue = useMemo(() => {
+    return { socket, disconnect, goDown, setGoDown }
+  }, [goDown, socket, disconnect]);
+
+  useEffect(() => {
+    console.log(socket, '--------------------------------------------------------socket???');
+    if (!socket) {
+      const socketInstance = io(`${process.env.NEXT_PUBLIC_BASE_URL}`, {
+        withCredentials: true,
+      });
+
+      socketInstance.on('connect', async () => {
+        console.log('소켓연결 성공!', socketInstance.id);
+
+        if (session?.user?.id) {
+          socketInstance.emit('login', { id: session.user.id });
+          console.log('-----------------------------------------------------socket emit login on connect')
+        }
+      })
+
+      setSocket(socketInstance);
+
+      // return () => {
+      //   console.log('------------------------------------------- socket.disconnect');
+      //   socketInstance?.disconnect();
+      // }
+    }
+  }, [session?.user?.id, socket]);
+
+  useEffect(() => {
+    if (socket?.connected && session?.user?.id) {
+      socket?.emit('login', { id: session?.user?.id });
+      console.log('------------------------------------------------------------socket emit login')
+    }
+
+    // return () => {
+    //   console.log('----------------------------------------------- socket.disconnect');
+    //   socket?.disconnect();
+    // }
+  }, [session?.user?.id, socket]);
+
+  return (
+    <socketContext.Provider value={contextValue}>
+      {children}
+    </socketContext.Provider>
+  )
+}
--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from 'react';
+import SocketProvider from './_components/SocketProvider';
+import { auth } from '@/auth';
 
 type Props = { children: ReactNode, modal: ReactNode };
 
-export default function Layout({ children, modal }: Props) {
+export default async function Layout({ children, modal }: Props) {
+  const session = await auth();
   return (
-    <>
+    <SocketProvider session={session}>
       {children}
       {modal}
-    </>
+    </SocketProvider>
   )
 }
