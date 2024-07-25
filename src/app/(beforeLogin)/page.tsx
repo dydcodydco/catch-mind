f9e50c915ad853c0e02400ec52b7c4db1f5cfc59
asd0905 <dydcodydco@daum.net>
2024-07-25 13:31:31 +0900

    Feat: 메인화면 수정

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -9,15 +9,16 @@ export default async function HomePage() {
     <div className="flex w-full justify-center items-center flex-col">
       <h1 className='font-extrabold text-4xl md:text-9xl md:leading-none -mt-20'>캐치마인드</h1>
       <div className='flex mt-8 gap-2 items-center'>
-        {session?.user?.name && (
+        {session?.user?.name ? (
           <>
             {session?.user?.name}님 환영합니다.
             <Button className='p-0 ml3'><Link href='/home' className='p-3'>고고</Link></Button>
           </>
+        ) : (
+          <Link href={'/login'}>
+            <Button>로그인</Button>
+          </Link>
         )}
-        <Link href={'/login'}>
-          <Button>로그인</Button>
-        </Link>
       </div>
     </div>
   );
