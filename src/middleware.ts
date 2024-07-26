f7399a3a9b28e93714dfd9b44f7e44fc4b9765c6
asd0905 <dydcodydco@daum.net>
2024-07-26 15:22:02 +0900

    Feat: 게임 방 url로 직접 들어갔을때 게임참여 모달로 이동

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export { auth } from "@/auth";
 
 export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
   console.log('------------------------------------middleware start ------------------------------------------');
   console.log('------------------------------------middleware end ------------------------------------------');
 
@@ -14,6 +15,17 @@ export async function middleware(request: NextRequest) {
   console.log(session?.user?.id, '--------------------middleware session id');
   if (session && path === '/login') {
     return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  
+  if (pathname.startsWith('/room/')) {
+    // 여기서 referer를 체크합니다
+    const referer = request.headers.get('referer')
+    
+    if (!referer || !referer.includes(request.headers.get('host') || '')) {
+      // 유효하지 않은 접근으로 간주하고 리다이렉트
+      return NextResponse.redirect(new URL('/join-room', request.url))
+    }
   }
 
   return NextResponse.next();
